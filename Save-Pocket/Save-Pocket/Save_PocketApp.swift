7d10ec1bc46e3d15ac5db420e3701e70b27848fa
Heshan <rajithaheshan1998@gmail.com>
2024-10-29 03:53:01 -0700

    create the  project

---
+++ Save-Pocket/Save-Pocket/Save_PocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  Save_PocketApp.swift
+//  Save-Pocket
+//
+//  Created by Heshan on 10/29/24.
+//
+
+import SwiftUI
+
+@main
+struct Save_PocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
